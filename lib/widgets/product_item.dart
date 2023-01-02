d802628f8ffcd3bfcbd0afcd0d58c600a70c72dd
eraloop <romanricakam@gmail.com>
2023-01-02 20:34:51 +0100

    added product detail screen,

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import "../screens/product_detail_screen.dart";
 class ProductItem extends StatelessWidget {
 
   final String id;
@@ -12,8 +13,14 @@ class ProductItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
-      child: GridTile(child: Image.network(imageUrl, fit: BoxFit.cover),
-      footer: GridTileBar(
+      child: GridTile(
+        child: GestureDetector(
+          onTap: (){
+            Navigator.of(context).push(MaterialPageRoute(builder:(ctx) => ProductDetailScreen(title)),);
+          },
+          child: Image.network(imageUrl, fit: BoxFit.cover),),
+       
+       footer: GridTileBar(
         backgroundColor: Colors.black87,
         title: Text(title, textAlign: TextAlign.center, ), 
         leading: IconButton(icon: Icon(Icons.favorite),
