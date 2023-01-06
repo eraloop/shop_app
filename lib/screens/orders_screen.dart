b767859f496b29087e3a0fc03bfd117ca78cb328
eraloop <romanricakam@gmail.com>
2023-01-06 05:56:58 +0100

    added the order screen

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -1,1 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../providers/orders.dart' show Order;
+import '../widgets/order_item.dart';
+
+class OrdersScreen extends StatelessWidget {
+  const OrdersScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final orderData = Provider.of<Order>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Your Orders"),
+      ),
+      body: ListView.builder(
+          itemCount: orderData.orders.length,
+          itemBuilder: (ctx, i) => OrderItem(orderData.orders[i])),
+    );
+  }
+}
---
+++ lib/widgets/order_item.dart
@@ -0,0 +1,31 @@
+import 'package:intl/intl.dart';
+import 'package:flutter/material.dart';
+
+import '../providers/orders.dart' as ord;
+
+class OrderItem extends StatelessWidget {
+  final ord.OrderItem order;
+
+  const OrderItem(this.order);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(10),
+      child: Column(children: [
+        ListTile(
+          title: Text('\$${order.amount}'),
+          subtitle: Text(
+            DateFormat('dd MM yyyy hh:mm').format(order.dateTime),
+          ),
+          trailing: IconButton(
+            icon: const Icon(
+              Icons.expand_more,
+            ),
+            onPressed: () {},
+          ),
+        )
+      ]),
+    );
+  }
+}
