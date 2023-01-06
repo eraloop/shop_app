96e268de7b8bb70214caeb5c089134be7426235b
eraloop <romanricakam@gmail.com>
2023-01-06 14:49:51 +0100

    done with snackbar

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -34,11 +34,16 @@ class ProductItem extends StatelessWidget {
                 icon: const Icon(Icons.shopping_cart),
                 onPressed: () {
                   cart.addItem(product.id, product.price, product.title);
+                  ScaffoldMessenger.of(context).hideCurrentSnackBar();
                   ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                      content: Text('Added item to card'),
-                      duration: Duration(seconds: 2),
-                    ),
+                    SnackBar(
+                        content: const Text('Added item to card'),
+                        duration: const Duration(seconds: 2),
+                        action: SnackBarAction(
+                            label: 'UNDO',
+                            onPressed: () {
+                              cart.removeSingleItem(product.id);
+                            })),
                   );
                 },
                 color: Theme.of(context).colorScheme.secondary,
